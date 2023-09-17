13449bb5620e44f62095ff8c0794ee35b4db2f65
AhsanBilal157 <Khanbilal6172@gmail.com>
2023-09-17 21:46:59 +0500

    updated images

--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -19,7 +19,7 @@ import React from "react";
  * Need an image? Check out https://unsplash.com to download a photo you
  * freely use on your site.
  */
-import image from "../images/Portfolio.jpeg";
+import image from "../images/projects_section.jpg";
 
 const imageAltText = "desktop with books and laptop";
 
